fc4ffc6326d0161aaf81e27d79897758fc41444e
JustAlittleWolf <jakobi5000@gmail.com>
2024-01-29 11:41:25 +0100

    Filter TranslatableTextContent from signs
    Implemented #1
    
    - Filter any `TranslatableTextContent` objects on signs that are not from the vanilla game. As there are quite a lot of translation keys, they are stored in a gzipped file `vanillaTranslationKeys.gzip`. On client startup this file gets read and stored into memory.
    - Bumped version to 1.1.0
    - Updated README to reflect the changes.

--- src/main/java/me/wolfii/moddetectionpreventer/mixin/AbstractSignEditScreenMixin.java
+++ src/main/java/me/wolfii/moddetectionpreventer/mixin/AbstractSignEditScreenMixin.java
@@ -1,6 +1,7 @@
 package me.wolfii.moddetectionpreventer.mixin;
 
 import me.wolfii.moddetectionpreventer.text.KeybindFilter;
+import me.wolfii.moddetectionpreventer.text.TranslationFilter;
 import net.minecraft.client.gui.screen.ingame.AbstractSignEditScreen;
 import net.minecraft.text.*;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,22 +15,29 @@ import java.util.stream.Stream;
 @Mixin(AbstractSignEditScreen.class)
 public class AbstractSignEditScreenMixin {
 
-    @Redirect(method = "<init>(Lnet/minecraft/block/entity/SignBlockEntity;ZZLnet/minecraft/text/Text;)V", at=@At(value = "INVOKE", target = "Ljava/util/stream/Stream;map(Ljava/util/function/Function;)Ljava/util/stream/Stream;"))
+    @Redirect(method = "<init>(Lnet/minecraft/block/entity/SignBlockEntity;ZZLnet/minecraft/text/Text;)V", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;map(Ljava/util/function/Function;)Ljava/util/stream/Stream;"))
     private Stream<String> removeCustomKeybinds(Stream<Text> instance, Function<? super Text, ? extends Text> function) {
         return instance.map(message -> filterKeybindsRecursive(message).getString());
     }
 
     @Unique
     private Text filterKeybindsRecursive(Text message) {
-        MutableText filtered;
+        MutableText filtered = MutableText.of(message.getContent());
         if (message.getContent() instanceof KeybindTextContent keybindTextContent) {
-            if (KeybindFilter.isVanillaKeybinding(keybindTextContent.getKey())) {
+            String keybind = keybindTextContent.getKey();
+            if (KeybindFilter.isVanillaKeybinding(keybind)) {
                 filtered = MutableText.of(keybindTextContent);
             } else {
-                filtered = MutableText.of(new PlainTextContent.Literal(keybindTextContent.getKey()));
+                filtered = MutableText.of(new PlainTextContent.Literal(keybind));
             }
-        } else {
-            filtered = MutableText.of(message.getContent());
+        }
+        if (message.getContent() instanceof TranslatableTextContent translatableTextContent) {
+            String translationKey = translatableTextContent.getKey();
+            if (TranslationFilter.isVanillaTranslation(translationKey)) {
+                filtered = MutableText.of(translatableTextContent);
+            } else {
+                filtered = MutableText.of(new PlainTextContent.Literal(translationKey));
+            }
         }
         filtered.setStyle(message.getStyle());
         for (Text sibling : message.getSiblings()) {
